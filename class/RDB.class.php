06e1999226cb169b26aa8dd52896be05bd557435
Moedrian <ikamitse@gmail.com>
2019-04-18 22:49:00 +0800

    add basic usage of simple inserting methods
    The execution of SQL were intent to set in DB classes, other classes
    do only logic interactions.

--- class/RDB.class.php
+++ class/RDB.class.php
@@ -3,17 +3,17 @@
 namespace Data\RDB;
 
 require '../vendor/autoload.php';
+require  '../config/Conf.php';
+use Conf;
 
 use PDO;
-use Conf;
 
 class RDB
 {
-    private $db_type = Conf::DB_SQL_NAME;
-    private $db_host = Conf::DB_SQL_HOST;
-    private $db_name = Conf::DB_SQL_DATABASE;
-    private $db_username = Conf::DB_SQL_USER;
-    private $db_password = Conf::DB_SQL_PASSWD;
+    private $db_type = Conf::RDB_NAME;
+    private $db_host = Conf::RDB_HOST;
+    private $db_username = Conf::RDB_USER;
+    private $db_password = Conf::RDB_PASSWD;
 
     public function __set($name, $value)
     {
@@ -25,14 +25,33 @@ class RDB
         echo $this->$name;
     }
 
-    public function dataLink()
+    /**
+     * @param $db_name
+     * @return PDO
+     */
+    public function dataLink($db_name)
     {
-        $dsn = $this->db_type . ":host=" . $this->db_host . ";dbname=" . $this->db_name;
+        $dsn = $this->db_type . ":host=" . $this->db_host . ";dbname=" . $db_name;
         $conn = new PDO($dsn, $this->db_username, $this->db_password);
         return $conn;
     }
 
-    public function addQueryGen($json) {
+    /**
+     * @param $table
+     * @param $json
+     *
+     * To add customers, products and simple orders only in vendor part
+     */
+    public function vendorSimpleAdd($table, $json) {
+
         $arr = json_decode($json, true);
+
+        $db = new RDB();
+        $conn = $db->dataLink(Conf::RDB_VENDOR_DB);
+
+        $conn->beginTransaction();
+        $stmt = $conn->prepare(Conf::Vendor_DB[$table]);
+        $stmt->execute(array_values($arr[$table]));
+        $conn->commit();
     }
 }
--- class/Vendor.class.php
+++ class/Vendor.class.php
@@ -8,20 +8,52 @@ use \Platform;
 
 class Vendor extends Platform
 {
+    /**
+     * @param $json
+     *
+     *  {
+     *  "customer": {
+     *      "cust_name": "Pop Team Epic",
+     *      "cust_contact": "Pipimi",
+     *      "cust_tel": "114-514-893",
+     *      "mail": "anime@kuso.com"
+     *      }
+     *  }
+     *
+     */
     public function addOrder($json)
     {
         // TODO: Implement addOrder() method.
-        $RDB = new RDB();
-        $db = $RDB->dataLink();
+        $conn = new RDB();
+        $conn->vendorSimpleAdd('orders', $json);
     }
 
+    /**
+     * @param $json
+     *
+     *
+     * 
+     */
     public function addCustomer($json)
     {
         // TODO: Implement addCustomer() method.
+        $conn = new RDB();
+        $conn->vendorSimpleAdd('customers', $json);
     }
 
     public function addDevice($json)
     {
         // TODO: Implement addDevice() method.
+    }
+
+    /**
+     * @param $json
+     *
+     */
+    public function addProduct($json)
+    {
+        // TODO: Implement addProduct() method.
+        $conn = new RDB();
+        $conn->vendorSimpleAdd('products', $json);
     }
 }

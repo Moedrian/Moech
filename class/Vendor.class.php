4946bb10c737f05884f0c793d23c4cf338ea9643
Moedrian <ikamitse@gmail.com>
2019-04-26 15:21:37 +0800

    fix bugs in SQL queries
    add a boolean column table_status into vendor.order_items to iden-
    tify if the param table is set or not

--- class/Vendor.class.php
+++ class/Vendor.class.php
@@ -15,6 +15,207 @@ use PDO;
 
 class Vendor extends Platform
 {
+    /**
+     * @param $json
+     *
+     */
+    public function addProduct($json)
+    {
+        $this->vendorSimpleAdd('products', $json);
+    }
+
+
+    /**
+     * @param string $json
+     *
+     *   {
+     *       "customer": {
+     *           "cust_name": "Pop Team Epic",
+     *           "cust_contact": "Pipimi",
+     *           "cust_tel": "114-514-893",
+     *           "mail": "anime@kuso.com"
+     *       }
+     *   }
+     *
+     *
+     */
+    public function addCustomer($json)
+    {
+        $this->vendorSimpleAdd('customers', $json);
+        $arr = json_decode($json, true);
+        $this->initCustomerDB($arr['customer']['cust_name']);
+    }
+
+
+    /**
+     * @param string $json
+     *
+     *   {
+     *       "cust_name": "Pop Team Epic",
+     *       "dev":{
+     *           "dev_1":{
+     *               "dev_id": "YJSP114",
+     *               "province": "Rust",
+     *               "city": "Utopia"
+     *           },
+     *           "dev_2":{
+     *               "dev_id": "YJSP514",
+     *               "province": "Jessie",
+     *               "city": "Lucy"
+     *           }
+     *       }
+     *   }
+     */
+    public function addDevice($json)
+    {
+        $arr = json_decode($json, true);
+
+        $cust_id = $this->getCustID($arr['cust_name']);
+
+        $val = "";
+        $vals = "";
+
+        // Create multi-insert query here
+        foreach ($arr['dev'] as $pk => $pv) {
+            foreach ($arr['dev'][$pk] as $ck => $cv) {
+                $val .= ('"' . $cv . '",');
+            }
+            $val = '(' . $val . $cust_id . ',"' . $arr['cust_name'] . '")';
+            $vals .= $val . ",";
+            $val = "";
+        }
+
+        $query = "insert into devices(dev_id, province, city, cust_id, cust_name) VALUES";
+        $query = substr($query . $vals, 0,-1);
+
+        $db = new RDB();
+        $conn = $db->dataLink(Conf::RDB_VENDOR_DB);
+
+        $conn->beginTransaction();
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        $conn->commit();
+    }
+
+
+    /**
+     * @param string $json
+     *
+     * Suppose the customer is already in database
+     *
+     *   {
+     *       "cust_name":"Pop Team Epic",
+     *       "orders":{
+     *           "item_1":{
+     *               "dev_id":"YJSP114",
+     *               "item":"param-middle",
+     *               "param":"rev",
+     *               "quantity":"2"
+     *           },
+     *           "item_2":{
+     *               "dev_id":"YJSP514",
+     *               "item":"param-high",
+     *               "param":"voltage_AB",
+     *               "quantity":"3"
+     *           }
+     *       }
+     *   }
+     *
+     */
+    public function addOrder($json)
+    {
+
+        $arr = json_decode($json, true);
+
+        $today = date('Y-m-d');
+        $cust_id = $this->getCustID($arr['cust_name']);
+
+        $db = new RDB();
+        $conn = $db->dataLink(Conf::RDB_VENDOR_DB);
+
+        $insert_orders_record = "insert into orders values(null, ?, ?)";
+
+        $conn->beginTransaction();
+
+        $stmt = $conn->prepare($insert_orders_record);
+        $stmt->execute([$today, $cust_id]);
+        $order_id = $conn->lastInsertId();
+
+        $vals = "";
+        $val = "";
+
+        foreach ($arr['orders'] as $pk => $pv) {
+            foreach ($arr['orders'][$pk] as $ck => $cv) {
+                $val .= ('"'. $cv . '",');
+                if ($ck == 'item')
+                    $price = $this->getProductPrice($cv);
+            }
+            $val = "(" . $order_id . "," . $val . $price . "),";
+            $vals .= $val;
+            $val = "";
+        }
+
+        $query = "insert into order_items(order_num, dev_id, item, param, quantity, price) VALUES";
+        $query = $query . $vals;
+        $query = substr($query . $vals, 0, -1);
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+
+        $conn->commit();
+
+        $this->initCustomerDevice($cust_id);
+    }
+
+
+    /**
+     * @param string $cust_name
+     */
+    private function initCustomerDB($cust_name)
+    {
+        $db = new RDB();
+        $conn = $db->dataLink(Conf::RDB_VENDOR_DB);
+
+        $cust_id = $this->getCustID($cust_name);
+
+        $conn->beginTransaction();
+
+        $query = "create database if not exists 'moni_" . $cust_id . " character set utf8mb4 collate utf8mb4_unicode_ci";
+        $conn->prepare($query)->execute();
+
+        $conn->commit();
+    }
+
+
+    /**
+     * @param string $cust_id
+     */
+    private function initCustomerDevice($cust_id)
+    {
+        $db = new RDB();
+        $conn = $db->dataLink('moni_' . $cust_id);
+
+        $conn->beginTransaction();
+
+        $query = "select dev_id from " .Conf::RDB_VENDOR_DB. ".devices where cust_id='" . $cust_id . "'";
+        $dev = $conn->query($query)->fetchAll(PDO::FETCH_COLUMN);
+
+        for ($i = 0; $i < count($dev); $i++) {
+            $query = "select dev_id, param from " .Conf::RDB_VENDOR_DB. ".order_items where dev_id='" . $dev[$i] . "'" . "and table_status=0";
+            $params = $conn->query($query)->fetchAll(PDO::FETCH_KEY_PAIR);
+            foreach ($params as $key => $value) {
+                $crt_query[] = "create table if not exists " .$key."_".$value. "(crt_time datetime(3) not null primary key, val float(6,2) not null) engine=MyISAM";
+                $crt_query[] = "update " . Conf::RDB_VENDOR_DB . ".order_items set table_status=1 where dev_id='".$key."' and param='". $value. "'";
+            }
+        }
+
+        for ($i = 0; $i < count($crt_query); $i++) {
+            $conn->prepare($crt_query[$i])->execute();
+        }
+
+        $conn->commit();
+
+    }
+
 
     /**
      * @param string $table
@@ -69,198 +270,4 @@ class Vendor extends Platform
         return $row->price;
     }
 
-    /**
-     * @param string $json
-     *
-     * Suppose the customer is already in database
-     *
-     *   {
-     *       "cust_name":"Pop Team Epic",
-     *       "orders":{
-     *           "item_1":{
-     *               "dev_id":"YJSP114",
-     *               "item":"param-middle",
-     *               "param":"rev",
-     *               "quantity":"2"
-     *           },
-     *           "item_2":{
-     *               "dev_id":"YJSP514",
-     *               "item":"param-high",
-     *               "param":"voltage-AB",
-     *               "quantity":"3"
-     *           }
-     *       }
-     *   }
-     *
-     */
-    public function addOrder($json)
-    {
-
-        $arr = json_decode($json, true);
-
-        $today = date('Y-m-d');
-        $cust_id = $this->getCustID($arr['cust_name']);
-
-        $db = new RDB();
-        $conn = $db->dataLink(Conf::RDB_VENDOR_DB);
-
-        $insert_orders_record = "insert into orders values(null, ?, ?)";
-
-        $conn->beginTransaction();
-
-        $stmt = $conn->prepare($insert_orders_record);
-        $stmt->execute([$today, $cust_id]);
-        $order_id = $conn->lastInsertId();
-
-        $vals = "";
-        $val = "";
-
-        foreach ($arr['orders'] as $pk => $pv) {
-            foreach ($arr['orders'][$pk] as $ck => $cv) {
-                $val .= ('"'. $cv . '",');
-                if ($ck == 'item')
-                    $price = $this->getProductPrice($cv);
-            }
-            $val = "(" . $order_id . "," . $val . $price . "),";
-            $vals .= $val;
-            $val = "";
-        }
-
-        $query = "insert into order_items(order_num, dev_id, item, param, quantity, price) VALUES";
-        $query = $query . $vals;
-        $query = substr($query . $vals, 0, -1);
-        $stmt = $conn->prepare($query);
-        $stmt->execute();
-
-        $conn->commit();
-
-        $this->initCustomerDevice($cust_id);
-    }
-
-    /**
-     * @param string $json
-     *
-     *   {
-     *       "customer": {
-     *           "cust_name": "Pop Team Epic",
-     *           "cust_contact": "Pipimi",
-     *           "cust_tel": "114-514-893",
-     *           "mail": "anime@kuso.com"
-     *       }
-     *   }
-     *
-     *
-     */
-    public function addCustomer($json)
-    {
-        $this->vendorSimpleAdd('customers', $json);
-        $arr = json_decode($json, true);
-        $this->initCustomerDB($arr['customer']['cust_name']);
-    }
-
-    /**
-     * @param string $json
-     *
-     *   {
-     *       "cust_name": "Pop Team Epic",
-     *       "dev":{
-     *           "dev_1":{
-     *               "dev_id": "YJSP114",
-     *               "province": "Rust",
-     *               "city": "Utopia"
-     *           },
-     *           "dev_2":{
-     *               "dev_id": "YJSP514",
-     *               "province": "Jessie",
-     *               "city": "Lucy"
-     *           }
-     *       }
-     *   }
-     */
-    public function addDevice($json)
-    {
-        $arr = json_decode($json, true);
-
-        $cust_id = $this->getCustID($arr['cust_name']);
-
-        $val = "";
-        $vals = "";
-
-        // Create multi-insert query here
-        foreach ($arr['dev'] as $pk => $pv) {
-            foreach ($arr['dev'][$pk] as $ck => $cv) {
-                $val .= ('"' . $cv . '",');
-            }
-            $val = '(' . $val . $cust_id . ',"' . $arr['cust_name'] . '")';
-            $vals .= $val . ",";
-            $val = "";
-        }
-
-        $query = "insert into devices(dev_id, province, city, cust_id, cust_name) VALUES";
-        $query = substr($query . $vals, 0,-1);
-
-        $db = new RDB();
-        $conn = $db->dataLink(Conf::RDB_VENDOR_DB);
-
-        $conn->beginTransaction();
-        $stmt = $conn->prepare($query);
-        $stmt->execute();
-        $conn->commit();
-    }
-
-    /**
-     * @param string $cust_name
-     */
-    private function initCustomerDB($cust_name)
-    {
-        $db = new RDB();
-        $conn = $db->dataLink(Conf::RDB_VENDOR_DB);
-
-        $cust_id = $this->getCustID($cust_name);
-
-        $conn->beginTransaction();
-
-        $query = "create database if not exists 'moni_" . $cust_id . " character set utf8mb4 collate utf8mb4_unicode_ci";
-        $conn->prepare($query)->execute();
-
-        $conn->commit();
-    }
-
-    /**
-     * @param string $cust_id
-     */
-    private function initCustomerDevice($cust_id)
-    {
-        $db = new RDB();
-        $conn = $db->dataLink('moni_' . $cust_id);
-
-        $conn->beginTransaction();
-
-        $query = "select dev_id from devices where cust_id='" . $cust_id . "'";
-        $dev = $conn->query($query)->fetchAll(PDO::FETCH_ASSOC);
-
-        for ($i = 0; $i < count($dev); $i++) {
-            $query = "select dev_id, param from order_items where dev_id='" . $dev[$i] . "'";
-            $params = $conn->query($query)->fetchAll(PDO::FETCH_KEY_PAIR);
-            foreach ($params as $key => $value) {
-                $crt_query[] = "create table if not exists " .$key."_".$value. "(crt_time datetime(3) not null primary key, val float(6,2) not null) engine=MyISAM";
-            }
-        }
-
-        for ($i = 0; $i < count($crt_query); $i++) {
-            $conn->prepare($crt_query[$i])->execute();
-        }
-
-        $conn->commit();
-
-    }
-
-    /**
-     * @param $json
-     *
-     */
-    public function addProduct($json)
-    {
-        $this->vendorSimpleAdd('products', $json);
-    }
 }

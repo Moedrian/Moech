e4649b2b2ee13efe5822a80151e0ba171ed56d4d
moedrian <ikamitse@gmail.com>
2019-05-13 21:36:14 +0800

    rewrite the init sql for deployment of vendor database

--- init/vendor.sql
+++ init/vendor.sql
@@ -4,14 +4,25 @@ CREATE DATABASE IF NOT EXISTS vendor DEFAULT CHARACTER SET utf8mb4 COLLATE utf8m
 
 USE vendor;
 
-CREATE TABLE customers (
+CREATE TABLE customer_info (
     cust_id int NOT NULL AUTO_INCREMENT,
     cust_name char(50) NOT NULL,
     cust_contact char(50) NOT NULL,
     cust_tel char(15) NOT NULL,
     cust_mail char(50) NULL,
-    PRIMARY KEY(cust_id)
-) ENGINE=InnoDB;
+    PRIMARY KEY (cust_id)
+) ENGINE = InnoDB;
+
+INSERT INTO customer_info (cust_id, cust_name, cust_contact, cust_tel, cust_mail) VALUES
+(10000, 'Nano Inc.', 'Nano', '12345678900', 'nichijou@mail.hakase.com');
+
+CREATE TABLE customer_reg (
+    username char(30) NOT NULL,
+    user_mail char(50) NOT NULL,
+    password varchar(60) NOT NULL,
+    cust_name char(50) NOT NULL,
+    PRIMARY KEY (username)
+) ENGINE = InnoDB;
 
 CREATE TABLE devices (
     dev_id char(20) NOT NULL,
@@ -19,8 +30,8 @@ CREATE TABLE devices (
     cust_name char(50) NOT NULL,
     province char(30) NOT NULL,
     city char(30) NOT NULL,
-    PRIMARY KEY(dev_id)
-) ENGINE=InnoDB;
+    PRIMARY KEY (dev_id)
+) ENGINE = InnoDB;
 
 CREATE TABLE orders (
     order_num int NOT NULL AUTO_INCREMENT,
@@ -28,6 +39,9 @@ CREATE TABLE orders (
     cust_id int NOT NULL,
     PRIMARY KEY(order_num)
 ) ENGINE=InnoDB;
+
+INSERT INTO orders (order_num, order_date, cust_id) VALUES
+(20000, '2019-04-01', 10000);
 
 CREATE TABLE order_items (
     seq_id int NOT NULL,
@@ -37,8 +51,9 @@ CREATE TABLE order_items (
     param char(30) NOT NULL,
     quantity int NOT NULL,
     price float(6,2) NOT NULL,
-    PRIMARY KEY(seq_id)
-) ENGINE=InnoDB;
+    table_status tinyint(10) NOT NULL DEFAULT 0,
+    PRIMARY KEY (seq_id)
+) ENGINE = InnoDB;
 
 CREATE TABLE product_param (
     category char(20) NOT NULL,
@@ -47,16 +62,16 @@ CREATE TABLE product_param (
     charging char(30) NOT NULL,
     price float(6,2) NOT NULL,
     description text,
-    PRIMARY KEY(category)
-) ENGINE=InnoDB;
+    PRIMARY KEY (category)
+) ENGINE = InnoDB;
 
 CREATE TABLE product_addition (
     category char(20) NOT NULL,
     charging char(30) NOT NULL,
     price float(6,2) NOT NULL,
     description text,
-    PRIMARY KEY(category)
-) ENGINE=InnoDB;
+    PRIMARY KEY (category)
+) ENGINE = InnoDB;
 
 CREATE TABLE params_ref (
     seq_id int NOT NULL AUTO_INCREMENT,
@@ -65,15 +80,9 @@ CREATE TABLE params_ref (
     freq int NOT NULL,
     min float(6,2),
     max float(6,2),
-    duration float,
+    abnormal_duration float,
     extra text,
-    PRIMARY KEY(seq_id)
-) ENGINE=InnoDB;
-
-INSERT INTO customers (cust_id, cust_name, cust_contact, cust_tel, cust_mail) VALUES
-    (10000, 'Ariadust', 'Tori', '12345678900', 'musashi@mail.com');
-
-INSERT INTO orders (order_num, order_date, cust_id) VALUES
-    (20000, '2019-04-01', 10000);
+    PRIMARY KEY (seq_id)
+) ENGINE = InnoDB;
 
 COMMIT;

8b89bcfb7d4e534b3f54b45564f20b22b517faff
moedrian <ikamitse@gmail.com>
2019-05-14 11:00:10 +0800

    fix bad grammer and add test msgs

--- init/vendor.sql
+++ init/vendor.sql
@@ -33,6 +33,18 @@ CREATE TABLE devices (
     PRIMARY KEY (dev_id)
 ) ENGINE = InnoDB;
 
+CREATE TABLE params_ref (
+    seq_id int NOT NULL AUTO_INCREMENT,
+    dev_id char(20) NOT NULL,
+    param char(30) NOT NULL,
+    freq int NOT NULL,
+    min float(6,2),
+    max float(6,2),
+    abnormal_duration float,
+    extra text,
+    PRIMARY KEY (seq_id)
+) ENGINE = InnoDB;
+
 CREATE TABLE orders (
     order_num int NOT NULL AUTO_INCREMENT,
     order_date date NOT NULL,
@@ -56,33 +68,33 @@ CREATE TABLE order_items (
 ) ENGINE = InnoDB;
 
 CREATE TABLE product_param (
-    category char(20) NOT NULL,
+    item char(20) NOT NULL,
     freq_min int,
     freq_max int,
     charging char(30) NOT NULL,
     price float(6,2) NOT NULL,
     description text,
-    PRIMARY KEY (category)
+    PRIMARY KEY (item)
 ) ENGINE = InnoDB;
 
+INSERT INTO product_param (item, freq_min, freq_max, charging, price, description) VALUES
+("param_small", 0, 100, "subscription/month", 200, "for params with a low frequency"),
+("param_middle" , 101, 500, "subscription/month", 400, "for params with a relatively high frequency"),
+("param_high" , 501, 800, "subscription/month", 700, "for params with a high frequency"),
+("param_extra" , 801, null, "subscription/month", 1000, "for params with an extremely high frequency");
+
 CREATE TABLE product_addition (
-    category char(20) NOT NULL,
+    item char(20) NOT NULL,
     charging char(30) NOT NULL,
     price float(6,2) NOT NULL,
     description text,
-    PRIMARY KEY (category)
+    PRIMARY KEY (item)
 ) ENGINE = InnoDB;
 
-CREATE TABLE params_ref (
-    seq_id int NOT NULL AUTO_INCREMENT,
-    dev_id char(20) NOT NULL,
-    param char(30) NOT NULL,
-    freq int NOT NULL,
-    min float(6,2),
-    max float(6,2),
-    abnormal_duration float,
-    extra text,
-    PRIMARY KEY (seq_id)
-) ENGINE = InnoDB;
+INSERT INTO product_addition (item, charging, price, description) VALUES
+("alarm", "subscription/calculation", 2, "the price is multiplied by this price and the amount of params per second"),
+("graph", "subscription/calculation", 5, "the price is multiplied by this price and the amount of params per second"),
+("dignosis", "subscription/calculation", 10, "the price is multiplied by this price and the amount of params per second");
+
 
 COMMIT;

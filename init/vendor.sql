b577bd42a43b9ecc31fcfe1c5b1c6e5e3d4bb1e6
moedrian <ikamitse@gmail.com>
2019-05-17 17:14:27 +0800

    add instance_list table and comments

--- init/vendor.sql
+++ init/vendor.sql
@@ -1,9 +1,24 @@
 START TRANSACTION;
 
+/**/
 CREATE DATABASE IF NOT EXISTS vendor DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 USE vendor;
 
+-- To store sign-up information.
+-- The very first table during orders' creating procedure.
+CREATE TABLE customer_sign_up (
+    username char(30) NOT NULL,
+    user_mail char(50) NOT NULL,
+    password varchar(60) NOT NULL,
+    cust_name char(50) NOT NULL,
+    PRIMARY KEY (username)
+) ENGINE = InnoDB;
+
+-- To store detailed information that is linked to the customer
+-- signed up before.
+-- After an account is created, the customer would be prompted
+-- essential information about the company.
 CREATE TABLE customer_info (
     cust_id int NOT NULL AUTO_INCREMENT,
     cust_name char(50) NOT NULL,
@@ -16,14 +31,8 @@ CREATE TABLE customer_info (
 INSERT INTO customer_info (cust_id, cust_name, cust_contact, cust_tel, cust_mail) VALUES
 (10000, 'Nano Inc.', 'Nano', '12345678900', 'nichijou@mail.hakase.com');
 
-CREATE TABLE customer_reg (
-    username char(30) NOT NULL,
-    user_mail char(50) NOT NULL,
-    password varchar(60) NOT NULL,
-    cust_name char(50) NOT NULL,
-    PRIMARY KEY (username)
-) ENGINE = InnoDB;
-
+-- To store devices to be monitored.
+-- The devices are associated to the customer company.
 CREATE TABLE devices (
     dev_id char(20) NOT NULL,
     cust_id int NOT NULL,
@@ -33,6 +42,8 @@ CREATE TABLE devices (
     PRIMARY KEY (dev_id)
 ) ENGINE = InnoDB;
 
+-- To store detailed parameters' detailed information.
+-- Also this is the base of orders' items.
 CREATE TABLE params_ref (
     seq_id int NOT NULL AUTO_INCREMENT,
     dev_id char(20) NOT NULL,
@@ -45,6 +56,7 @@ CREATE TABLE params_ref (
     PRIMARY KEY (seq_id)
 ) ENGINE = InnoDB;
 
+-- To store simple order information.
 CREATE TABLE orders (
     order_num int NOT NULL AUTO_INCREMENT,
     order_date date NOT NULL,
@@ -55,6 +67,8 @@ CREATE TABLE orders (
 INSERT INTO orders (order_num, order_date, cust_id) VALUES
 (20000, '2019-04-01', 10000);
 
+-- To store items in the orders created before.
+-- When the table in MySQL is created, the table_status will be set to 1.
 CREATE TABLE order_items (
     seq_id int AUTO_INCREMENT NOT NULL,
     dev_id char(20) NOT NULL,
@@ -71,6 +85,20 @@ CREATE TABLE order_items (
 INSERT INTO order_items (seq_id, dev_id, order_num, category, item, param, quantity, price, table_status) VALUES
 (1, 'ONE', 20000, 'ONE', 'ONE', 'ONE', 1, 0, 0);
 
+-- To store instance status for deployment.
+-- One customer may have more than one instance depending on the scale.
+-- After the instance is ready,
+-- deployment_status and config_status will be set to 1
+CREATE TABLE instance_list (
+    instance_id int NOT NULL AUTO_INCREMENT,
+    cust_name char(50) NOT NULL,
+    cust_id int NOT NULL,
+    deployment_status tinyint(10) NOT NULL DEFAULT 0,
+    config_status tinyint(10) NOT NULL DEFAULT 0,
+    PRIMARY KEY (instance_id)
+);
+
+-- To store the index of param monitoring products.
 CREATE TABLE product_param (
     item char(20) NOT NULL,
     freq_min int,
@@ -87,6 +115,7 @@ INSERT INTO product_param (item, freq_min, freq_max, charging, price, descriptio
 ('param_high' , 501, 800, 'subscription/month', 700, 'for params with a high frequency'),
 ('param_extra' , 801, null, 'subscription/month', 1000, 'for params with an extremely high frequency');
 
+-- To store the index of param monitoring additional services.
 CREATE TABLE product_addition (
     item char(20) NOT NULL,
     charging char(30) NOT NULL,
@@ -99,6 +128,5 @@ INSERT INTO product_addition (item, charging, price, description) VALUES
 ('alarm', 'subscription/calculation', 2, 'the price is multiplied by this price and the amount of params per second'),
 ('graph', 'subscription/calculation', 5, 'the price is multiplied by this price and the amount of params per second'),
 ('diagnosis', 'subscription/calculation', 10, 'the price is multiplied by this price and the amount of params per second');
-
 
 COMMIT;

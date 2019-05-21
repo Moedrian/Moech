ab2cbea1ba3b39142fd6aaf39b74f5de216f159b
moedrian <ikamitse@gmail.com>
2019-05-21 13:54:48 +0800

    Add class DeployInstance and rewrite Abstraction
    No database operation allowed in DeployInstance, it's just a tool
    So now implement comes before abstraction first now
    VERY PRIMARY DESU

--- src/Abstraction/DeployBackend.abstract.php
+++ src/Abstraction/DeployBackend.abstract.php
@@ -6,22 +6,30 @@ namespace Moech\AbstractClass;
 
 abstract class DeployBackend
 {
+    // THERE ISN'T ANY DATABASE OPERATION IN THIS CLASS
+
     /**
-     * @param string $filename the name of configuration file to be created
-     * @param string $path the path to place the file
+     * @param int $instance_id
+     *
+     * To create directories for a new instance
+     */
+    abstract public function generateDir(int $instance_id);
+
+
+    /**
+     * @param int $instance_id the name of configuration file to be created
      * @param string $json information to be placed into the configuration
      *
      * To generate the config files for a new instance, Platform or DeviceManufacturer
      */
-    abstract public function generateConfigFile(string $filename, string $path, string $json);
+    abstract public function generateConfigFile(int $instance_id, string $json);
 
     /**
-     * @param string $config_path
      * @param string $sql_path
      *
-     * To create Relative Databases
+     * To create Relative Databases instance
      */
-    abstract public function initializeMainDatabase(string $config_path, string $sql_path);
+    abstract public function initializeDatabase(string $sql_path);
 
     /**
      * @param string $config_path

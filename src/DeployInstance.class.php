1fec9e1124f24be55dd4bfae2598bdf3a2f65269
moedrian <ikamitse@gmail.com>
2019-05-21 13:54:48 +0800

    Add class DeployInstance and rewrite Abstraction
    No database operation allowed in DeployInstance, it's just a tool
    So now implement comes before abstraction first now
    VERY PRIMARY DESU

--- src/DeployInstance.class.php
+++ src/DeployInstance.class.php
@@ -3,63 +3,87 @@
 
 namespace Moech\Deploy;
 
-// Abstract class to be extended
-require __DIR__ . "/Abstraction/Deployment.abstract.php";
-
-require __DIR__ . "/RDB.class.php";
-
 require __DIR__ . "/../vendor/autoload.php";
 
-use Moech\AbstractClass\Deployment;
-use Moech\Data\RDB;
+// Abstract class to be extended
+use Moech\AbstractClass\DeployBackend;
+
 use Zend\Config\Config as Config;
 use Zend\Config\Writer\Ini as Ini;
 
-use PDO;
 
-class DeployInstance extends Deployment
+class DeployInstance extends DeployBackend
 {
 
     /**
-     * @param string $dirname the name of configuration file to be created
-     * The filename shall be like {@example cust_name_cust_id_order_date}
-     * @param string $path the path to place the file
+     * @param int $instance_id
+     *
+     * To create directories for a new instance
+     */
+    public function generateDir(int $instance_id)
+    {
+        chmod(__DIR__ . "/../deploy", 0755);
+        $pathname = __DIR__ . "/../deploy/instance_" . $instance_id;
+        mkdir($pathname, 0755, true);
+
+        // Create directories to place codes, etc.
+        $dir_array = ["src", "html", "config", "log", "assets", "api"];
+        foreach ($dir_array as $item) {
+            mkdir($pathname . "/" . $item, 0755, true);
+        }
+
+        // Copy composer.json there, after uploaded, `composer install` shall be executed
+        copy(__DIR__ . "/../composer.json", $pathname . "/composer.json");
+    }
+
+
+    /**
+     * @param int $instance_id the name of configuration file to be created
+     * The filename shall be like {@example instance_114514.ini}
+     *
      * @param string $json information to be placed into the configuration
      *
      * @uses Config()
      * @uses Ini()
      *
      * To generate the config files for a new instance, Platform or DeviceManufacturer
+     *
+     * NOT RELATED TO ORDER NUMBER OR CUSTOMER YET
+     *
      * @attention be sure to check privilege first
      *
      * @example test/json_input/config.json
      */
-    public function generateConfigFile(string $dirname, string $path, string $json)
+    public function generateConfigFile(int $instance_id, string $json)
     {
         chmod(__DIR__ . "/../deploy", 0755);
-        mkdir(__DIR__ . "/../deploy/instance_" . $dirname, 0755, true);
 
         $config = new Config(json_decode($json, true));
         $writer = new Ini();
 
-        $filename = __DIR__ . "/../deploy/instance_" . $dirname ."/config.ini";
+        // as $this->generateDir() suggests
+        $filename = __DIR__ . "/../deploy/instance_" . $instance_id ."/config/config.ini";
+
         $fp = fopen($filename, "w");
         fwrite($fp, $writer->toString($config));
         fclose($fp);
     }
 
+
     /**
-     * @param string $config_path
-     * @param string $sql_path
+     * @param string $sql_pathname to specify the path of sql file
      *
      * To create Relative Databases
+     *
+     * @return mixed $init the sql file for initialization
      */
-    public function initializeMainDatabase(string $config_path, string $sql_path)
+    public function initializeDatabase(string $sql_pathname = __DIR__ . "/../init/customer.sql")
     {
-        // TODO: Implement initializeMainDatabase() method.
-        $db = new RDB();
-        $conn = $db->dataLink()
+        $init = file_get_contents($sql_pathname);
+
+        return $init;
     }
+
 
     /**
      * @param string $config_path

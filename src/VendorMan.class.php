b046cfde4bd658ccd29e6888cf561902b3003bfd
moedrian <ikamitse@gmail.com>
2019-05-22 15:03:15 +0800

    Add methods to VendorMan
    add VendorMan::setInstanceStatusTrue()
    add VendorMan::addInstanceConfig()
    adjust comment format

--- src/VendorMan.class.php
+++ src/VendorMan.class.php
@@ -11,11 +11,73 @@ use Moech\Deploy\DeployInstance;
 use PDO;
 use PDOException;
 
+/**
+ * For the management workflow after the data is added into the database
+ */
 class VendorMan
 {
 
-    public function allocateInstanceToCustomers()
+    /**
+     * USE AFTER INSTANCE CREATION
+     *
+     * After the config file is generated,
+     * `instances.cfg_status` will be set to true
+     *
+     * After the instance get occupied by certain customer,
+     * `instances.dep_status` will be set to true. Meanwhile,
+     * column `instances.cust_id` & `instances.cust_name` will be filled
+     *
+     * Column `instances.load_status` is a
+     * @todo mf!
+     *
+     * @param int $instance_id
+     * @param string $status "deploy" || config || load
+     *
+     * @throws PDOException
+     */
+    public function setInstanceStatusTrue(int $instance_id, string $status)
     {
+        $set_query = "Empty Query";
+
+        if ($status == "deploy") {
+            $set_query = "update instances set dep_status=1 where instance_id=?";
+        } elseif ($status == "config") {
+            $set_query = "update instances set dep_status=1 where instance_id=?";
+        } elseif ($status == "load") {
+            $set_query = "update instances set dep_status=1 where instance_id=?";
+        }
+
+        $conn = new ReDB("vendor");
+
+        try {
+            $conn->prepare($set_query)->execute([$instance_id]);
+        } catch (PDOException $e) {
+            $conn->errorLogWriter($e);
+        }
+    }
+
+    /**
+     * Prepares for the config file
+     *
+     * Not bound to customer, yet
+     *
+     * @param int $instance_id comes from {@see VendorAdd::AddInstance()}
+     * @param string $json {@example test/json_input/config.json}
+     */
+    public function addInstanceConfig(int $instance_id, string $json)
+    {
+        $dep = new DeployInstance();
+        $dep->generateConfigFile($instance_id, $json);
+
+        // After the config is well prepared, set the status to true
+        $this->setInstanceStatusTrue($instance_id, "config");
+    }
+
+
+
+    public function allocateInstanceToCustomers(string $cust_name, int $cust_id, int $instance_id)
+    {
+
     }
 
     public function allocateInstanceForDevices(array $devices)
@@ -23,11 +85,14 @@ class VendorMan
     }
 
     /**
-     * @param int $instance_id
+     * Create database fo
      *
      * After an order was created, this function shall be executed
      * Scan that instance_list
      * Meanwhile this function requires a ready-to-go server instance
+     *
+     * @param int $instance_id
+     * @throws PDOException
      */
     public function initiateDatabase(int $instance_id)
     {
@@ -36,6 +101,7 @@ class VendorMan
         $dep = new DeployInstance();
 
         try {
+            // Create basic databases for customer side management
             $cust_conn->prepare($dep->initializeDatabase())->execute();
         } catch (PDOException $e) {
             $cust_conn->errorLogWriter($e);
@@ -44,7 +110,7 @@ class VendorMan
         $vendor_conn = new ReDB("vendor");
 
         try {
-
+            // Create databases for devices under certain customer
             $vendor_conn->beginTransaction();
 
             $query = "select dev_id from devices where cust_id=(select cust_id from instances where instance_id=?)";

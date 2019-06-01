54a43e5f0e614a057f4d9c324cee2a8abd5fa9cb
moedrian <ikamitse@gmail.com>
2019-06-01 19:59:52 +0800

    add drafts for data transferring

---
+++ src/interfaces/DataConveyInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Moech\Interfaces;
+
+/**
+ * Manipulates devices runtime data
+ *
+ * @method void goInReDB(int $instance_id, string $json)
+ * @method void goInNoDB(int $instance_id, string $json)
+ * @method array goOutReDB(int $instance_id, string $json)
+ * @method array goOutNoDB(int $instance_id, string $json)
+ */
+interface DataConveyInterface
+{
+
+}
---
+++ src/interfaces/NoDBInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Moech\Interfaces;
+
+
+/**
+ * @method void setHashData(array $data)
+ * @method void setSerialData(array $data)
+ * @method void getHashData()
+ * @method void getSerialData(int $amount)
+ */
+class NoDBInterface
+{
+
+}

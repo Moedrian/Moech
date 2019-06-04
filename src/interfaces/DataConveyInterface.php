4ef2a4935be0b644c355e1a8b7d23f1c5a20800b
moedrian <ikamitse@gmail.com>
2019-06-04 23:32:13 +0800

    add an interface to instruct data flow handling

--- src/interfaces/DataConveyInterface.php
+++ src/interfaces/DataConveyInterface.php
@@ -6,10 +6,11 @@ namespace Moech\Interfaces;
 /**
  * Manipulates devices runtime data
  *
- * @method void goInReDB(int $instance_id, string $json)
- * @method void goInNoDB(int $instance_id, string $json)
- * @method array goOutReDB(int $instance_id, string $json)
- * @method array goOutNoDB(int $instance_id, string $json)
+ * @method void     goInReDB(int $instance_id, string $json)
+ * @method void     goInNoDB(int $instance_id, string $json)
+ * @method array    goOutReDB(int $instance_id, string $json)
+ * @method array    goOutNoDB(int $instance_id, string $json)
+ * @method string   queryGlue(array $values)
  */
 interface DataConveyInterface
 {

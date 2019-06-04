1eaaf8e920153420e690e06ea1e13148c1c9ac92
moedrian <ikamitse@gmail.com>
2019-06-04 23:29:52 +0800

    a workerman for raspi, only sql now

---
+++ api/receiver_sample.php
@@ -0,0 +1,27 @@
+<?php
+
+set_time_limit(0);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Workerman\Worker;
+use Moech\RaspiDataConvey;
+
+$ws_worker = new Worker('tcp://xxx.xxx.xxx.xxx:xxxxx');
+
+$ws_worker->count = 4;
+
+$ws_worker->onConnect = static function ($connection)
+{
+    $connection->onMessage = static function ($connection, $data)
+    {
+        $data = json_decode($data, true);
+        $testRun = new RaspiDataConvey();
+
+        $queries = $testRun->queryGlue($data);
+        $testRun->goInReDB($queries['ReDB']);
+        $connection->send('Successfully received!');
+    };
+};
+
+Worker::runAll();

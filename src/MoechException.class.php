1e0b9a413ce1f9c0c4d40e47d23dea70aeebb55c
moedrian <ikamitse@gmail.com>
2019-06-13 13:47:50 +0800

    a simple af prototype of custom Exception

--- src/MoechException.class.php
+++ src/MoechException.class.php
@@ -4,10 +4,26 @@
 namespace Moech\Exception;
 
 use Exception;
-
+use Throwable;
 
 class MoechException extends Exception
 {
-    private $log_dir = __DIR__ . '/../log';
+    private $log_dir = __DIR__ . '/../log/error.log';
 
+
+    public function __construct($message = '', $code = 0, Throwable $previous = null)
+    {
+        chmod($this->log_dir, 0755);
+        parent::__construct($message, $code, $previous);
+    }
+
+
+    public function writeErrorLog(object $exception): void
+    {
+        $fp = fopen($this->log_dir, 'a+b');
+
+        fwrite($fp, $exception->getMessage() . PHP_EOL);
+
+        fclose($fp);
+    }
 }
